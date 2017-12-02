b9c9a64ec168782163cdb8123352868798af2bbb
Travis Rich <travis.s.rich@gmail.com>
2017-12-01 21:36:39 -0500

    Refactor and linting

--- graphdb.js
+++ graphdb.js
@@ -5,9 +5,19 @@ if (process.env.NODE_ENV !== 'production') {
 	require('./config.js');
 }
 
-const driver = neo4j.driver(process.env.GRAPHENEDB_BOLT_URL, neo4j.auth.basic(process.env.GRAPHENEDB_BOLT_USER, process.env.GRAPHENEDB_BOLT_PASSWORD));
+const driver = neo4j.driver(
+	process.env.GRAPHENEDB_BOLT_URL,
+	neo4j.auth.basic(
+		process.env.GRAPHENEDB_BOLT_USER,
+		process.env.GRAPHENEDB_BOLT_PASSWORD
+	)
+);
 const graphSession = driver.session();
-graphSession.run('CREATE INDEX ON :Thing(identifier) CREATE CONSTRAINT ON (t:Thing) ASSERT t.identifier IS UNIQUE');
+graphSession.run(`
+	CREATE INDEX ON :Thing(identifier)
+	CREATE CONSTRAINT ON (t:Thing) ASSERT t.identifier IS UNIQUE
+`);
+
 export default {
 	graphSession: graphSession
 };

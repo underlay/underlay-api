c56cd072f58fd14a98206640bab897e25c2efd8a
Travis Rich <travis.s.rich@gmail.com>
2017-12-21 18:39:30 -0500

    NewRelic seems to work automatically. Removing cruft

--- worker.js
+++ worker.js
@@ -104,169 +104,167 @@ const schemaSpec = ajv.addKeyword('identifierIsValid', {
 
 function processTask(channel) {
 	return (msg)=> {
-		// nr.startBackgroundTransaction('workerProcess', ()=> {
-			const body = JSON.parse(msg.content.toString());
-			const assertions = body.assertions;
-			const requestId = body.requestId;
-			const webhookUri = body.webhookUri;
-			const assertionDate = body.assertionDate;
+		const body = JSON.parse(msg.content.toString());
+		const assertions = body.assertions;
+		const requestId = body.requestId;
+		const webhookUri = body.webhookUri;
+		const assertionDate = body.assertionDate;
 
-			console.log(` [x] Received ${requestId}`);
-			console.time(` [x] Finished ${requestId}`);
+		console.log(` [x] Received ${requestId}`);
+		console.time(` [x] Finished ${requestId}`);
 
-			/* Ensure that every assertion has a type value */
-			const allHaveType = assertions.reduce((prev, curr)=> {
-				if (!curr.type) { return false; }
-				return prev;
-			}, true);
-			/* If not all assertions have a type, return user error */
-			if (!allHaveType) {
-				return sendResultToWebhook(webhookUri, requestId, 'Type required', null);
-			}
+		/* Ensure that every assertion has a type value */
+		const allHaveType = assertions.reduce((prev, curr)=> {
+			if (!curr.type) { return false; }
+			return prev;
+		}, true);
+		/* If not all assertions have a type, return user error */
+		if (!allHaveType) {
+			return sendResultToWebhook(webhookUri, requestId, 'Type required', null);
+		}
 
-			/* Create validation calls. Update the assertion to not
-			have a type (which is not part of the node schema) */
-			const validations = assertions.map((item)=> {
-				const nodeData = { ...item };
-				delete nodeData.type;
-				return schemaSpec.validate(item.type, nodeData);
+		/* Create validation calls. Update the assertion to not
+		have a type (which is not part of the node schema) */
+		const validations = assertions.map((item)=> {
+			const nodeData = { ...item };
+			delete nodeData.type;
+			return schemaSpec.validate(item.type, nodeData);
+		});
+
+		return Promise.all(validations)
+		.then((data)=> {
+			/* Add the type back to the assertions and ensure an identifier is provided
+			or created. Add an assertionDate. */
+			const results = data.map((item, index)=> {
+				return {
+					...item,
+					type: assertions[index].type,
+					identifier: assertions[index].identifier || uuidv4(),
+					assertionDate: assertionDate,
+				};
 			});
 
-			return Promise.all(validations)
-			.then((data)=> {
-				/* Add the type back to the assertions and ensure an identifier is provided
-				or created. Add an assertionDate. */
-				const results = data.map((item, index)=> {
-					return {
-						...item,
-						type: assertions[index].type,
-						identifier: assertions[index].identifier || uuidv4(),
-						assertionDate: assertionDate,
-					};
+			/* Create a bulk cypher statement by iterating over each assertion */
+			const cypherStatement = results.reduce((prev, currAssertion, index)=> {
+				const schemaDef = schemaSpec._schemas[currAssertion.type].schema;
+				const schemaProperties = schemaDef.properties;
+
+				/* Split the assertion attributes into those that will
+				create attributes on a node and those that will create
+				a relationship */
+				const attributes = {};
+				const relationships = {};
+				Object.keys(currAssertion).forEach((key)=> {
+					const item = schemaProperties[key];
+					if (item && key !== 'identifier') {
+						if (item.identifierIsValid || (item.items && item.items.identifierIsValid)) {
+							relationships[key] = currAssertion[key];
+						} else {
+							attributes[key] = currAssertion[key];
+						}
+					}
 				});
 
-				/* Create a bulk cypher statement by iterating over each assertion */
-				const cypherStatement = results.reduce((prev, currAssertion, index)=> {
-					const schemaDef = schemaSpec._schemas[currAssertion.type].schema;
-					const schemaProperties = schemaDef.properties;
+				/* Create the cypher statement that will set all of the attributes to the node */
+				const attributeSetString = generateSetString(`item${index}`, attributes);
 
-					/* Split the assertion attributes into those that will
-					create attributes on a node and those that will create
-					a relationship */
-					const attributes = {};
-					const relationships = {};
-					Object.keys(currAssertion).forEach((key)=> {
-						const item = schemaProperties[key];
-						if (item && key !== 'identifier') {
-							if (item.identifierIsValid || (item.items && item.items.identifierIsValid)) {
-								relationships[key] = currAssertion[key];
-							} else {
-								attributes[key] = currAssertion[key];
-							}
-						}
-					});
+				/* Create the cypher statement that will set all
+				of the relationships defined in the assertion */
+				const relationshipSetString = Object.keys(relationships).reduce((prevString, currKey)=> {
+					/* Set relationship destination node type. e.g. :Thing:Person */
+					/* We can just always have the destination nodetype be :Thing
+					since we validate the ids earlier */
+					const destinationNodeType = ':Thing';
+					let currString;
 
-					/* Create the cypher statement that will set all of the attributes to the node */
-					const attributeSetString = generateSetString(`item${index}`, attributes);
-
-					/* Create the cypher statement that will set all
-					of the relationships defined in the assertion */
-					const relationshipSetString = Object.keys(relationships).reduce((prevString, currKey)=> {
-						/* Set relationship destination node type. e.g. :Thing:Person */
-						/* We can just always have the destination nodetype be :Thing
-						since we validate the ids earlier */
-						const destinationNodeType = ':Thing';
-						let currString;
-
-						/* If there is an array of uuids, iterate over each to generate set statement
-						If there is only one uuid, simply create the set statement */
-						if (Array.isArray(relationships[currKey])) {
-							currString = relationships[currKey].reduce((prevKeyString, currKeyIdentifier, currKeyIndex)=> {
-								/* Generate set string params. e.g. 'item.date = 5, item.name="fish"' */
-								const setStringParameters = generateSetString(`r${currKeyIndex}`, currKeyIdentifier);
-								/* Generate set string itself. */
-								const setString = typeof currKeyIdentifier === 'string' || !setStringParameters
-									? ''
-									: `SET ${setStringParameters}`;
-
-								/* Get identifier of destination node */
-								const toIdentifier = typeof currKeyIdentifier === 'string'
-									? currKeyIdentifier
-									: currKeyIdentifier.identifier;
-
-								/* Compile setString with identifiers for cypher query */
-								return `${prevKeyString}
-									${prevKeyString ? 'WITH *' : ''}
-									MATCH (from${currKeyIndex}${schemaDef.cypherLabels} { identifier: '${currAssertion.identifier}' }),(to${currKeyIndex}${destinationNodeType} { identifier: '${toIdentifier}' })
-									MERGE (from${currKeyIndex})-[r${currKeyIndex}:${currKey}]->(to${currKeyIndex})
-									${setString}
-								`;
-							}, '');
-						} else {
+					/* If there is an array of uuids, iterate over each to generate set statement
+					If there is only one uuid, simply create the set statement */
+					if (Array.isArray(relationships[currKey])) {
+						currString = relationships[currKey].reduce((prevKeyString, currKeyIdentifier, currKeyIndex)=> {
 							/* Generate set string params. e.g. 'item.date = 5, item.name="fish"' */
-							const setStringParameters = generateSetString('r', relationships[currKey]);
+							const setStringParameters = generateSetString(`r${currKeyIndex}`, currKeyIdentifier);
 							/* Generate set string itself. */
-							const setString = typeof relationships[currKey] === 'string' || !setStringParameters
+							const setString = typeof currKeyIdentifier === 'string' || !setStringParameters
 								? ''
 								: `SET ${setStringParameters}`;
-							currString = `
-								MATCH (from${schemaDef.cypherLabels} { identifier: '${currAssertion.identifier}' }),(to${destinationNodeType} { identifier: '${currAssertion[currKey]}' })
-								MERGE (from)-[r:${currKey}]->(to)
+
+							/* Get identifier of destination node */
+							const toIdentifier = typeof currKeyIdentifier === 'string'
+								? currKeyIdentifier
+								: currKeyIdentifier.identifier;
+
+							/* Compile setString with identifiers for cypher query */
+							return `${prevKeyString}
+								${prevKeyString ? 'WITH *' : ''}
+								MATCH (from${currKeyIndex}${schemaDef.cypherLabels} { identifier: '${currAssertion.identifier}' }),(to${currKeyIndex}${destinationNodeType} { identifier: '${toIdentifier}' })
+								MERGE (from${currKeyIndex})-[r${currKeyIndex}:${currKey}]->(to${currKeyIndex})
 								${setString}
 							`;
-						}
-
-						return `${prevString}
-							${currString}
+						}, '');
+					} else {
+						/* Generate set string params. e.g. 'item.date = 5, item.name="fish"' */
+						const setStringParameters = generateSetString('r', relationships[currKey]);
+						/* Generate set string itself. */
+						const setString = typeof relationships[currKey] === 'string' || !setStringParameters
+							? ''
+							: `SET ${setStringParameters}`;
+						currString = `
+							MATCH (from${schemaDef.cypherLabels} { identifier: '${currAssertion.identifier}' }),(to${destinationNodeType} { identifier: '${currAssertion[currKey]}' })
+							MERGE (from)-[r:${currKey}]->(to)
+							${setString}
 						`;
-					}, '');
+					}
 
-					/* If there is an attributeSetString, generate the full cypher query */
-					const attributeMergeString = attributeSetString
-						? `MERGE (item${index}${schemaDef.cypherLabels} { identifier: "${currAssertion.identifier}" })
-							ON CREATE SET ${attributeSetString}
-							ON MATCH SET ${attributeSetString}`
-						: '';
-
-					return `${prev}
-						${attributeMergeString}
-						${attributeSetString && relationshipSetString ? 'WITH *' : ''}
-						${relationshipSetString}
+					return `${prevString}
+						${currString}
 					`;
 				}, '');
 
-				/* Return the results and run the cypher statement to update the graph db */
-				return Promise.all([results, graphSession.run(cypherStatement)]);
-			})
-			.then(([results])=> {
-				/* Add the processed assertions to the SQL list of assertions */
-				const sequelizeObjects = results.map((item)=> {
-					return {
-						assertion: item,
-						nodeId: item.identifier,
-						nodeType: item.type,
-						createdAt: assertionDate,
-						modifiedAt: assertionDate,
-					};
-				});
-				return Promise.all([results, Assertion.bulkCreate(sequelizeObjects)]);
-			})
-			.then(([results])=> {
-				/* Return the processed assertions with a success status */
-				return sendResultToWebhook(webhookUri, requestId, null, results);
-			})
-			.catch((err)=> {
-				console.log('Error processing assertion', err);
-				if (err.message !== 'Webhook URI not available') {
-					return sendResultToWebhook(webhookUri, requestId, err, null);
-				}
-				return null;
-			})
-			.finally(()=> {
-				console.timeEnd(` [x] Finished ${requestId}`);
-				channel.ack(msg);
+				/* If there is an attributeSetString, generate the full cypher query */
+				const attributeMergeString = attributeSetString
+					? `MERGE (item${index}${schemaDef.cypherLabels} { identifier: "${currAssertion.identifier}" })
+						ON CREATE SET ${attributeSetString}
+						ON MATCH SET ${attributeSetString}`
+					: '';
+
+				return `${prev}
+					${attributeMergeString}
+					${attributeSetString && relationshipSetString ? 'WITH *' : ''}
+					${relationshipSetString}
+				`;
+			}, '');
+
+			/* Return the results and run the cypher statement to update the graph db */
+			return Promise.all([results, graphSession.run(cypherStatement)]);
+		})
+		.then(([results])=> {
+			/* Add the processed assertions to the SQL list of assertions */
+			const sequelizeObjects = results.map((item)=> {
+				return {
+					assertion: item,
+					nodeId: item.identifier,
+					nodeType: item.type,
+					createdAt: assertionDate,
+					modifiedAt: assertionDate,
+				};
 			});
-		// });
+			return Promise.all([results, Assertion.bulkCreate(sequelizeObjects)]);
+		})
+		.then(([results])=> {
+			/* Return the processed assertions with a success status */
+			return sendResultToWebhook(webhookUri, requestId, null, results);
+		})
+		.catch((err)=> {
+			console.log('Error processing assertion', err);
+			if (err.message !== 'Webhook URI not available') {
+				return sendResultToWebhook(webhookUri, requestId, err, null);
+			}
+			return null;
+		})
+		.finally(()=> {
+			console.timeEnd(` [x] Finished ${requestId}`);
+			channel.ack(msg);
+		});
 	};
 }
 
